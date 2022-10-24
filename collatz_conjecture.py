8b17bc98499e1024335cdefc60f1dab39f7171ee
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-10-24 21:22:12 +0200

    Update collatz_conjecture.py

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,7 +1,7 @@
 def steps(number):
     steps = 0
     if(number==1):
-        return 1
+        return 0
     if(number < 1):
         raise ValueError("Only positive numbers are allowed")
     while(number>1):
