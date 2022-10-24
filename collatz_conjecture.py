5238a04652a76a571081d32b2c0fd43f29612d9e
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-10-24 21:20:49 +0200

    Update collatz_conjecture.py

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,9 +1,9 @@
 def steps(number):
     steps = 0
-    if(number == 0):
-        return 0
+    if(number==1):
+        return 1
     if(number < 1):
-        raise ValueError("Input is Negative")
+        raise ValueError("Only positive numbers are allowed")
     while(number>1):
         if((number % 2) == 0):
             number = (number/2)
